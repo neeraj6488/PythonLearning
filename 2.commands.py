cee489a1a2df8a92b224d9f1009160b9f989dd17
Neeraj Deshbhratar <neeraj6488@gmail.com>
2026-09-07 18:45:00 +0530

    added data types

--- 2.commands.py
+++ 2.commands.py
@@ -6,3 +6,21 @@ y=20 # this is a value
 result = x + y
 
 print(result)
+
+"""
+This is a multi line comment
+created by literals using double quote.
+"""
+
+'''
+This is a multi line comment
+created by literals using single quote.
+'''
+
+# commented out
+x=10 # this is a value
+y=20 # this is a value
+
+result = x + y
+
+print(result)
---
+++ 3.basic data types.py
@@ -0,0 +1,18 @@
+# Personal Information
+
+name = "Neeraj"
+age = 40
+height = 5.8
+is_student = False
+
+
+# Print Information
+print("Name: ", name)
+print("Age: ", age)
+print("Height: ", height)
+print("Are you a Student ", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
